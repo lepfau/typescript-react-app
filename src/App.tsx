5460d8590e1565922791db428e635a1449aeb678
lepfau <edouard.pfauwadel@gmail.com>
2021-11-17 10:24:26 +0100

    ff

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { TextField } from './TextField';
 
 const App: React.FC = () => {
+
+const [bool, setBool] = useState<boolean>(false)
+
+const handleClick = () => {
+setBool(!bool)
+}
+
+
   return (
   <div>
-    <TextField text='hello i am props'/>
+    <TextField text='hello i am props' subtext={33} istrue={bool}
+    fn={handleClick}/>
   </div>
   )
 }
--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 
 interface Props {
-    text: string;
+  text: string;
+  subtext: number;
+  istrue?: boolean;
+  fn: () => void
 }
 
-export const TextField: React.FC<Props> = ({text}) => {
-    return (
-        <div>
-            <h1>hello im textfield</h1>
-            <h2>{text}</h2>
-        </div>
-    )
-}
+
+export const TextField: React.FC<Props> = ({ text, subtext, istrue, fn }) => {
+  return (
+    <div>
+      <h1>hello im textfield</h1>
+      <h2>{text}</h2>
+      <h2>This is subtext number: {subtext}</h2>
+      {istrue && <h3>I display if true</h3>}
+      <button onClick={() => fn()}>Click</button>
+    </div>
+  );
+};
